6d8c8533e571496d1533f2c584aa0dac47d590cc
Cody <cchinothai@gmail.com>
2025-11-12 16:53:00 -0800

    configure eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,30 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
+import reactPlugin from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs,jsx}"], 
-    plugins: [
-      "react"
-    ], 
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      react: reactPlugin,
+      js,
+    },
     extends: [
-      "js/recommended", 
+      "js/recommended",
       "plugin:react/recommended"
-    ], 
-    languageOptions: { globals: globals.browser } 
-},
-  pluginReact.configs.flat.recommended,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: "detect", // auto-detect your React version
+      },
+    },
+    rules: {
+      // Customize rules here
+      "react/prop-types": "off", // example common override if you don't use prop-types
+    },
+  },
 ]);
