6256f563cab9a31ea5e7226b7b346b60bb118022
Cody <cchinothai@gmail.com>
2025-11-21 15:38:21 -0800

    frontend docker bind/cache optimization

--- frontend/src/CodyProfile.jsx
+++ frontend/src/CodyProfile.jsx
@@ -3,7 +3,8 @@ import { Card, Anchor, Avatar } from 'antd';
 
 const CodyProfile = () => {
     const { Link } = Anchor;
-
+    
+    
     return (
         <Card stye={{ width: '25%', textAlign: 'center'}}>
             <h1 data-testid="cody-chinothai">Cody Chinothai</h1>
